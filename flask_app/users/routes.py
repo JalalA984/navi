3854384d3524a0bf7fbf6f01616d82f2388ea759
Jalal <128756237+JalalA984@users.noreply.github.com>
2024-12-05 03:08:46 -0500

    implement logout and accountsettings functionality, TODO search bar and news blueprint

--- flask_app/users/routes.py
+++ flask_app/users/routes.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, redirect, url_for, render_template, flash, request
-from flask_login import current_user
+from flask_login import current_user, login_required, login_user, logout_user
 
 from ..models import User
 from flask_app.extensions import bcrypt
 
 
-from ..forms import RegistrationForm
+from ..forms import RegistrationForm, LoginForm
 
 users = Blueprint("users", __name__)
 
@@ -30,3 +30,40 @@ def register():
             return redirect(url_for('users.index'))
         
     return render_template('register.html', form=form, title="NaviNews | Register")
+
+
+@users.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return render_template('account.html')
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.objects(username=form.username.data).first()  
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return render_template('account.html')
+        else:
+            flash("Login unsuccessful. Authenticate again.", "danger")
+
+    return render_template('login.html', form=form, title="NaviNews | Login")
+
+
+
+@users.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('users.index'))
+
+
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    greeting = f"Hello, {current_user.username}!"
+
+    return render_template(
+        'account.html', title="NaviNews | Settings",
+        greeting=greeting
+    )
